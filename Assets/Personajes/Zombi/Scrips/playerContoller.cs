e846d7340d2c8aa6e83a7cbddf2788af90a05e78
droicer <162638445+droicer@users.noreply.github.com>
2025-04-11 08:58:48 -0500

    Descripción de tus cambios

--- Assets/Personajes/Zombi/Scrips/playerContoller.cs
+++ Assets/Personajes/Zombi/Scrips/playerContoller.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class playerContoller : MonoBehaviour
 {
     public float speed = 5f;
+    public float fuerzaSalto = 7f;
     private Rigidbody2D rb;
     private bool mirandoDerecha = true;
+
+    public Transform verificadorSuelo;
+    public float radioSuelo = 0.2f;
+    public LayerMask capaSuelo;
+
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); // Conectamos el Animator
     }
 
     void FixedUpdate()
@@ -18,7 +26,17 @@ public class playerContoller : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
 
-        // Verifica si hay movimiento en X
+        // Actualiza el parámetro Velocidad para controlar la animación
+        animator.SetFloat("Velocidad", Mathf.Abs(moveX));
+
+        // Salto
+        bool enSuelo = Physics2D.OverlapCircle(verificadorSuelo.position, radioSuelo, capaSuelo);
+        if (Input.GetKeyDown(KeyCode.Space) && enSuelo)
+        {
+            rb.AddForce(new Vector2(0, fuerzaSalto), ForceMode2D.Impulse);
+        }
+
+        // Girar sprite
         if (moveX > 0 && !mirandoDerecha)
         {
             Girar();
@@ -33,7 +51,16 @@ public class playerContoller : MonoBehaviour
     {
         mirandoDerecha = !mirandoDerecha;
         Vector3 escala = transform.localScale;
-        escala.x *= -1; // invierte la dirección del sprite
+        escala.x *= -1;
         transform.localScale = escala;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (verificadorSuelo != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(verificadorSuelo.position, radioSuelo);
+        }
     }
 }
